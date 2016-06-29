d5122879305f4063cfbedb0c1a9d2b28b31e16eb
virtualprodigyllc <virtualprodigyllc@gmail.com>
2016-06-28 21:39:58 -0400

    Initial commit. The project is not in a running state yet.  I just wanted to get the source I have already up on Git because this mac is a bit old.
    - Currently working on setting up the acceptance of custom fonts.
    - I also need to add the dialog to display the  notice for the font library I'm using.
    
    Going to create Develop and a bug fix branch

---
+++ BetterTextViewSample/bettertextview/src/main/java/com/virtualprodigy/bettertextview/BetterTextView.java
@@ -0,0 +1,83 @@
+package com.virtualprodigy.bettertextview;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * Created by virtualprodigyllc on 6/28/16.
+ */
+
+public class BetterTextView extends TextView {
+    private boolean scaleWithinBounds;
+    private String typefaceAsset;
+    private Context context;
+
+    public BetterTextView(Context context) {
+        super(context);
+        init(context, null);
+    }
+
+    public BetterTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs);
+    }
+
+    public BetterTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context, attrs);
+    }
+
+    /**
+     * Initializes the BetterTextView
+     * handles reading the custom attrs
+     *
+     * @param context
+     */
+    private void init(Context context, AttributeSet attrs) {
+        this.context = context;
+        TypedArray typedArray = context.getTheme().obtainStyledAttributes(
+                attrs,
+                R.styleable.betterTextViewAttrs,
+                0, 0);
+
+        try {
+            scaleWithinBounds = typedArray.getBoolean(R.styleable.betterTextViewAttrs_typefaceAsset, false);
+            typefaceAsset = typedArray.getString(R.styleable.betterTextViewAttrs_typefaceAsset);
+        } finally {
+            typedArray.recycle();
+        }
+
+        setTypeface();
+    }
+
+    /**
+     * Sets whether the TextView should scale to fit within its bounds
+     *
+     * @param scaleWithinBounds
+     */
+    public void setScaleWithinBounds(boolean scaleWithinBounds) {
+        this.scaleWithinBounds = scaleWithinBounds;
+    }
+
+    /**
+     * Allows the TypeFace to be set. Type faces are assumed to be stored in
+     * assets/fonts so only the TTF files name is needed
+     *
+     * @param typefaceAsset - Name of the TTF file
+     */
+    public void setTypefaceAsset(String typefaceAsset) {
+        this.typefaceAsset = typefaceAsset;
+        setTypeface();
+    }
+
+    /**
+     * Local method to call the parent set type face
+     */
+    private void setTypeface() {
+        Typeface font = Typeface.createFromAsset(context.getAssets(), typefaceAsset);
+        setTypeface(font);
+    }
+}

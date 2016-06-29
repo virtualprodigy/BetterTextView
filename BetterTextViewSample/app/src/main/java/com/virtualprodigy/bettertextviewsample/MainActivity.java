85a42bef380c874cbb440e38ab48373b4c69d740
virtualprodigyllc <virtualprodigyllc@gmail.com>
2016-06-29 02:23:04 -0400

    - Fixed the bug with the xmlns. Android studio didn't automatically add the "compile project(':bettertextview') so the mapping to my library was never created - moved my font resources to the correct folder. -  Tested the library. It works like a charm

--- BetterTextViewSample/app/src/main/java/com/virtualprodigy/bettertextviewsample/MainActivity.java
+++ BetterTextViewSample/app/src/main/java/com/virtualprodigy/bettertextviewsample/MainActivity.java
@@ -1,13 +1,17 @@
 package com.virtualprodigy.bettertextviewsample;
 
+import android.content.Context;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView helloWorld;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
